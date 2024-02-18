4d44b3a93b7e501d03316ad57da67f7400f72aee
rb9898 <robinboban998@gmail.com>
2024-02-18 17:32:14 +0530

    add product api service

---
+++ Store.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.ProductAPI.Data;
+using Store.Services.ProductAPI.Models;
+using Store.Services.ProductAPI.Models.Dto;
+
+namespace Store.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    [Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private IMapper _mapper;
+        private ResponseDto _responseDto;
+
+        public ProductAPIController(AppDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> result = _appDbContext.Products.ToList();
+                _responseDto.Result = _mapper.Map<IEnumerable<ProductDto>>(result);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product result = _appDbContext.Products.FirstOrDefault(u=> u.ProductId == id);
+                _responseDto.Result = _mapper.Map<ProductDto>(result);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _appDbContext.Add(obj);
+                _appDbContext.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _appDbContext.Update(obj);
+                _appDbContext.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product obj = _appDbContext.Products.First(u => u.ProductId == id);
+                _appDbContext.Remove(obj);
+                _appDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
---
+++ Store.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Store.Services.ProductAPI.Models;
+using Store.Services.ProductAPI.Models.Dto;
+
+namespace Store.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Product,ProductDto>();
+                config.CreateMap<ProductDto, Product>();
+            });
+            return mappingConfig;
+        }
+    }
+}
