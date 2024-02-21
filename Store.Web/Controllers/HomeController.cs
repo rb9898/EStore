2c47ef10aa399fea59bce5cb7b10b7bff05e317e
rb9898 <robinboban998@gmail.com>
2024-02-22 01:31:50 +0530

    add product views in home page

--- Store.Web/Controllers/HomeController.cs
+++ Store.Web/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Store.Web.Models;
+using Store.Web.Service.IService;
 using System.Diagnostics;
 
 namespace Store.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> list = new();
+            ResponseDto? responseDto = await _productService.GetAllProductAsync();
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(responseDto.Result.ToString());
+            }
+            else
+            {
+                TempData["error"] = responseDto.Message;
+            }
+            return View(list);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int productId)
+        {
+            ProductDto model = new();
+            ResponseDto? responseDto = await _productService.GetProductByIdAsync(productId);
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(responseDto.Result.ToString());
+            }
+            else
+            {
+                TempData["error"] = responseDto.Message;
+            }
+            return View(model);
         }
 
         public IActionResult Privacy()
