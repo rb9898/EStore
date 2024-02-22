49a853ababb542badd91e72cfddb457ab8bde9dc
rb9898 <robinboban998@gmail.com>
2024-02-22 23:02:32 +0530

    add shopping cart api

---
+++ Store.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Services.ShoppingCartAPI.Models;
+namespace Store.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+    }
+}
